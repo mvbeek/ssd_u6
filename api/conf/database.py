b1364e352aabfa8b3e12fc866117ab8d603a48a2
Shota Kameyama <shotakame@gmail.com>
2022-02-20 01:47:42 +0900

    Refactor environment env and remove hard coded DB info

--- api/conf/database.py
+++ api/conf/database.py
@@ -1,9 +1,37 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+VALUE_ERROR_MSG = "No %s set for the Env Variable. Go to README for more info."
+
+# Database Configuration
+DATABASE_USER = os.environ.get("DATABASE_USER")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+DATABASE_HOST = os.environ.get("DATABASE_HOST")
+DATABASE_PORT = os.environ.get("DATABASE_PORT")
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
+
+if not DATABASE_USER:
+    raise ValueError(VALUE_ERROR_MSG % "DATABASE_USER")
+if not DATABASE_PASSWORD:
+    raise ValueError(VALUE_ERROR_MSG % "DATABASE_PASSWORD")
+if not DATABASE_HOST:
+    raise ValueError(VALUE_ERROR_MSG % "DATABASE_HOST")
+# if not DATABASE_PORT:
+#     raise ValueError(VALUE_ERROR_MSG % "DATABASE_PORT")
+if not DATABASE_NAME:
+    raise ValueError(VALUE_ERROR_MSG % "DATABASE_NAME")
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' \
+                            + DATABASE_USER+':' \
+                            + DATABASE_PASSWORD+'@' \
+                            + DATABASE_HOST+'/' \
+                            + DATABASE_NAME
+
+
 # PyMySQL
-engine = create_engine('mysql+pymysql://root@localhost/u6')
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
--- api/conf/security.py
+++ api/conf/security.py
@@ -1,12 +1,7 @@
 import os
 from datetime import timedelta
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-if not SECRET_KEY:
-    raise ValueError("No SECRET_KEY set for Flask application")
-SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT")
-if not SECURITY_PASSWORD_SALT:
-    raise ValueError("No SECURITY_PASSWORD_SALT set for Flask application")
+VALUE_ERROR_MSG = "No %s set for the Env Variable. Go to README for more info."
 
 
 class BaseConfig():
@@ -15,8 +10,17 @@ class BaseConfig():
 
     # the most recommended password hash algorithm by OWASP
     SECURITY_PASSWORD_HASH = 'argon2'  # nosec
-    SECRET_KEY = SECRET_KEY
-    SECURITY_PASSWORD_SALT = SECURITY_PASSWORD_SALT
+
+    # Security Key
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    if not SECRET_KEY:
+        raise ValueError(VALUE_ERROR_MSG % "SECRET_KEY")
+
+    # Password Salt
+    SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT")
+    if not SECURITY_PASSWORD_SALT:
+        raise ValueError(VALUE_ERROR_MSG % "SECURITY_PASSWORD_SALT")
+
     SECURITY_REGISTERABLE = True
     SECURITY_SEND_REGISTER_EMAIL = False
     # If we implement TOTP. TBD
