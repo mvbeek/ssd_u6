4b0458be6e994c884350588750b3e4950b4d41bf
Shota Kameyama <shotakame@gmail.com>
2022-02-23 11:04:12 +0900

    added doc comments to security file

--- api/conf/security.py
+++ api/conf/security.py
@@ -1,9 +1,16 @@
+'''
+This file takes care of security settings.
+'''
 import os
 
 VALUE_ERROR_MSG = "No {} set for the Env Variable. Go to README for more info."
 
 
 class BaseConfig():
+    '''
+    BaseConfig class that contains the base configuration.
+    Applied to all evironments.
+    '''
     DEBUG = False
     TESTING = False
 
@@ -20,7 +27,9 @@ class BaseConfig():
     if not SECURITY_PASSWORD_SALT:
         raise ValueError(VALUE_ERROR_MSG.format("SECURITY_PASSWORD_SALT"))
 
+    # User registration function is activated.
     SECURITY_REGISTERABLE = True
+    # Send registration email function is deactivated.
     SECURITY_SEND_REGISTER_EMAIL = False
     # If we implement TOTP. TBD
     SECURITY_TOTP_ISSUER = 'SSD Flask Security TOTP'
@@ -36,17 +45,30 @@ class BaseConfig():
     '''
     SECURITY_PASSWORD_NORMALIZE_FORM = 'NFKD'  # nosec
 
-    # Store User Tracking Data
+    # Store User Tracking Data such as IP, last login time, etc.
     SECURITY_TRACKABLE = True
     SECURITY_CSRF_IGNORE_UNAUTH_ENDPOINTS = False
 
 
 class DevelopmentConfig(BaseConfig):
+    '''
+    DevelopmentConfig class that contains the development environment configuration.
+    '''
     DEBUG = True
     TESTING = True
     FLASK_ENV = 'development'
 
 
 class TestingConfig(BaseConfig):
+    '''
+    TestingConfig class that contains the testing environment configuration.
+    '''
     DEBUG = False
     TESTING = True
+
+class ProductionConfig(BaseConfig):
+    '''
+    ProductionConfig class that contains the production environment configuration.
+    '''
+    DEBUG = False
+    TESTING = False
