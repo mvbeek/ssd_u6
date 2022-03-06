6184e7cea7b3ce069352f1bb0643c1d776fa2c46
Matvb <83800387+mvbeek@users.noreply.github.com>
2022-03-06 08:47:54 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-username = "admin"
-password = "password"
+USERNAME=admin
+PASSWORD=admin
+GUEST_USERNAME=guest
+GUEST_PASSWORD=['dashboardguest!', 'second_pw!']
+SECURITY_TOKEN=cc83733cb0af8b884ff6577086b87909
